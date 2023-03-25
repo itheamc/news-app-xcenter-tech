282d187af82a31261f2267cbbf33a2c476abe1a6
Amit Chaudhary <itheamc@gmail.com>
2023-03-25 17:34:24 +0545

    added fields on the model

--- lib/ui/views/news/models/news_response_entity.dart
+++ lib/ui/views/news/models/news_response_entity.dart
@@ -1,4 +1,23 @@
+import 'news.dart';
 
 class NewsResponseEntity {
+  final String? status;
+  final int? totalResults;
+  final List<News> news;
 
+  /// Constructor
+  NewsResponseEntity({
+    this.status,
+    this.totalResults,
+    this.news = const [],
+  });
+
+  /// Factory method to convert json data to the NewsResponseEntity
+  factory NewsResponseEntity.fromJson(Map<String, dynamic> json) {
+    return NewsResponseEntity(
+      status: json['status'],
+      totalResults: json['totalResults'],
+      news: json['news'],
+    );
+  }
 }
--- lib/ui/views/news/models/news_source.dart
+++ lib/ui/views/news/models/news_source.dart
@@ -1,4 +1,56 @@
+import '../../../../utils/log_utils.dart';
 
 class NewsSource {
+  final String? id;
+  final String? name;
+  final String? description;
+  final String? url;
+  final String? category;
+  final String? language;
+  final String? country;
 
+  /// Constructor
+  NewsSource({
+    this.id,
+    this.name,
+    this.description,
+    this.url,
+    this.category,
+    this.language,
+    this.country,
+  });
+
+  /// Factory method to convert the Json data to the NewsSource object
+  factory NewsSource.fromJson(Map<String, dynamic> json) {
+    return NewsSource(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      url: json['url'],
+      category: json['category'],
+      language: json['language'],
+      country: json['country'],
+    );
+  }
+}
+
+/// Method to parse the news sources json data
+Future<List<NewsSource>?> parseNewsSourcesJsonData(dynamic data) async {
+  if (data == null || data is! List) return null;
+
+  final temp = List<NewsSource>.empty(growable: true);
+  for (final json in data) {
+    try {
+      temp.add(NewsSource.fromJson(json));
+    } catch (e) {
+      LogUtil.debug(
+        message: e.toString(),
+        functionName: 'parseNewsSourcesJsonData',
+        className: "Global",
+      );
+      continue;
+    }
+  }
+
+  return temp;
 }
