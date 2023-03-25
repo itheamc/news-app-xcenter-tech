606e4b6421cc445115b1949ce3757d61e4e4937a
Amit Chaudhary <itheamc@gmail.com>
2023-03-25 17:34:12 +0545

    changed base url and endpoints

--- lib/core/api/endpoints.dart
+++ lib/core/api/endpoints.dart
@@ -1,5 +1,9 @@
 class EndPoints {
-  static const String baseUrl = "https://base-url.com";
+  static const String baseUrl = "https://newsapi.org";
 
-  static const String banners = "/banners";
+  static const _version = "/v2";
+
+  static const String sources = "$_version/top-headlines/sources";
+  static const String topHeadlines = "$_version/top-headlines";
+  static const String news = "$_version/everything";
 }
