176edc20d929ff551c33467b4fb1f8a71b4fa120
Amit Chaudhary <itheamc@gmail.com>
2023-03-25 20:18:38 +0545

    minor change in data getter

--- lib/core/handlers/network/network_response.dart
+++ lib/core/handlers/network/network_response.dart
@@ -15,9 +15,7 @@ class NetworkResponse {
 
   bool get isSuccess => responseType == ResponseType.success;
 
-  dynamic get data => _response.data != null && _response.data is Map
-      ? _response.data['terms']
-      : null;
+  dynamic get data => _response.data;
 
   int? get statusCode => _response.statusCode;
 
