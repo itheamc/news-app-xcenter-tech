6c5cb6abc11a3f39f5edd923e193660fca9d3708
Amit Chaudhary <itheamc@gmail.com>
2023-03-29 21:32:51 +0545

    added responsive ui for news screen

--- lib/ui/views/news/news_screen.dart
+++ lib/ui/views/news/news_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
+import 'package:news_app_xcenter_tech/ui/shared/no_data_or_error_container.dart';
 import 'package:news_app_xcenter_tech/ui/shared/responsive_ui.dart';
 import 'package:news_app_xcenter_tech/ui/views/news/controller/news_controller.dart';
 import 'package:news_app_xcenter_tech/ui/views/news/models/news.dart';
+import 'package:news_app_xcenter_tech/utils/extension_functions.dart';
 
 import '../../shared/image_view.dart';
 import '../../shared/loading_indicator.dart';
@@ -19,7 +21,12 @@ class NewsScreen extends StatefulWidget {
 
 class _NewsScreenState extends State<NewsScreen>
     with ResponsiveUiMixin<NewsController> {
+  /// Getter for news source
   String get source => widget.source ?? 'bbc-news';
+
+  /// Getter for screen title
+  String? get title =>
+      widget.source?.replaceAll("-", " ").capitalizeFirstOfEach;
 
   @override
   void initState() {
@@ -29,6 +36,7 @@ class _NewsScreenState extends State<NewsScreen>
     });
   }
 
+  /// Method to load news as per the given source
   Future<void> _loadNews() async {
     controller.fetchNews(
       params: News.queryParams(
@@ -47,55 +55,21 @@ class _NewsScreenState extends State<NewsScreen>
   Widget ui4Phone({Key? key}) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(controller.title),
+        title: Text(title ?? controller.title),
         centerTitle: true,
       ),
-      body: Obx(() {
-        if (controller.fetchingNews) {
-          return const LoadingIndicator(
-            size: 36.0,
-            lineWidth: 3.0,
-            label: "Loading News...",
-          );
-        }
-
-        return ListView.builder(
+      body: _ui4Body(
+        newsItems: ListView.builder(
           itemCount: controller.listOfNews.length,
           itemBuilder: (BuildContext context, int index) {
             final news = controller.listOfNews[index];
             return Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-              child: InkWell(
-                onTap: () {
-                  context.go('/news/$source/${news.id}', extra: news);
-                },
-                borderRadius: BorderRadius.circular(16.0),
-                child: Ink(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  child: Column(
-                    children: [
-                      AspectRatio(
-                        aspectRatio: 18 / 9,
-                        child: NetworkImageView(
-                          url: news.coverUrl ?? "",
-                          fit: BoxFit.cover,
-                          borderRadius: BorderRadius.circular(16.0),
-                        ),
-                      ),
-                      Text(
-                        news.title ?? "",
-                        style: context.textTheme.titleMedium,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              padding: const EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 0.0),
+              child: _ui4NewsItem(news),
             );
           },
-        );
-      }),
+        ),
+      ),
     );
   }
 
@@ -103,8 +77,25 @@ class _NewsScreenState extends State<NewsScreen>
   Widget? ui4Desktop({Key? key}) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(controller.title),
+        title: Text(title ?? controller.title),
         centerTitle: true,
+      ),
+      body: _ui4Body(
+        newsItems: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 480.0,
+                mainAxisSpacing: 20.0,
+                crossAxisSpacing: 20.0,
+                childAspectRatio: 18 / 12),
+            itemBuilder: (BuildContext context, int index) {
+              final news = controller.listOfNews[index];
+              return _ui4NewsItem(news);
+            },
+            itemCount: controller.listOfNews.length,
+          ),
+        ),
       ),
     );
   }
@@ -113,23 +104,85 @@ class _NewsScreenState extends State<NewsScreen>
   Widget? ui4Tablet({Key? key}) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(controller.title),
+        title: Text(title ?? controller.title),
         centerTitle: true,
+      ),
+      body: _ui4Body(
+        newsItems: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20.0,
+              crossAxisSpacing: 20.0,
+              childAspectRatio: 18 / 12.5,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              final news = controller.listOfNews[index];
+              return _ui4NewsItem(news);
+            },
+            itemCount: controller.listOfNews.length,
+          ),
+        ),
       ),
     );
   }
 
-  @override
-  Widget? ui4Watch({Key? key}) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(controller.title),
-        centerTitle: true,
-      ),
-      body: Container(
-        color: Colors.orange,
-        child: const Center(
-          child: Text("I am watch"),
+  /// UI for body
+  Widget _ui4Body({
+    required Widget newsItems,
+  }) {
+    return Obx(() {
+      if (controller.fetchingNews) {
+        return const LoadingIndicator(
+          size: 36.0,
+          lineWidth: 3.0,
+          label: "Loading News...",
+        );
+      }
+
+      if (controller.error != null) {
+        return NoDataOrErrorContainer(
+          message: controller.error,
+        );
+      }
+
+      return newsItems;
+    });
+  }
+
+  /// Ui for the news item
+  Widget _ui4NewsItem(News news) {
+    return InkWell(
+      onTap: () {
+        context.push('/news/$source/${news.id}', extra: news);
+      },
+      borderRadius: BorderRadius.circular(16.0),
+      child: Ink(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 18 / 9,
+              child: NetworkImageView(
+                url: news.coverUrl ?? "",
+                fit: BoxFit.cover,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              news.title ?? "",
+              style: context.textTheme.titleMedium,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
         ),
       ),
     );
